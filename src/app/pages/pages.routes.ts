71f0c9889d25a1c936da8c5912333b59191de88f
Adro Ornelas <a22300918@ceti.mx>
2026-03-11 22:44:50 -0600

    Added source files

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { Documentation } from './documentation/documentation';
+import { Crud } from './crud/crud';
+import { Empty } from './empty/empty';
+
+import { Catalog } from './catalog/catalog';
+
+export default [
+    { path: 'documentation', component: Documentation },
+    { path: 'crud', component: Crud },
+    { path: 'empty', component: Empty },
+    { path: 'catalog', component: Catalog },
+    { path: '**', redirectTo: '/notfound' }
+] as Routes;
