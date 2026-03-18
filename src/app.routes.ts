4071f382b520bf8fb4191abd03214db0e91e5557
Adro Ornelas <a22300918@ceti.mx>
2026-03-18 09:57:29 -0600

    Not used components deleted

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
-import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
-import { Notfound } from './app/pages/notfound/notfound';
 import { Catalog } from './app/pages/catalog/catalog';
 
 export const appRoutes: Routes = [
@@ -12,13 +8,8 @@ export const appRoutes: Routes = [
         component: AppLayout,
         children: [
             { path: '', component: Catalog },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') },
         ]
     },
-    { path: 'landing', component: Landing },
-    { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,16 +1,9 @@
 import { Routes } from '@angular/router';
-import { Documentation } from './documentation/documentation';
-import { Crud } from './crud/crud';
-import { Empty } from './empty/empty';
- 
 import { Catalog } from './catalog/catalog';
 import { Profile } from './profile/profile';
 import { Shoppingcart } from './shoppingcart/shoppingcart';
 
 export default [
-    { path: 'documentation', component: Documentation },
-    { path: 'crud', component: Crud },
-    { path: 'empty', component: Empty },
     { path: 'catalog', component: Catalog },
     { path: 'profile', component: Profile },
     { path: 'shoppingcart', component: Shoppingcart },
