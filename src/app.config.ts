7bdc736b48dd49c6d35a0a7fce762d2a88927029
Adro Ornelas <a22300918@ceti.mx>
2026-03-26 21:57:41 -0600

    Now retrieves products from database
    Changed products.service.ts so now the products info is retrieved from the database and not from a xml file

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,11 +5,15 @@ import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch()),
         provideZonelessChangeDetection(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+
+        // Enable HttpClient
+        provideHttpClient(),
     ]
 };
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Product } from '../models/product.model';
@@ -7,46 +7,57 @@ import { Product } from '../models/product.model';
     providedIn: 'root'
 })
 export class ProductService {
+
+    private http = inject(HttpClient);
+
+    private apiUrl = `http://localhost:3000/api/products`;
+    getProducts():Observable<Product[]> {
+        return this.http.get<Product[]>(this.apiUrl);
+    }
+
+
+    // XTML THING
+
     // Replace with the actual path to your XML file
-    private xmlUrl = '/images/productos.xml';
+    // private xmlUrl = '/images/productos.xml';
 
-    constructor(private http: HttpClient) {}
+    // constructor(private http: HttpClient) {}
 
-    getProducts(): Observable<Product[]> {
-        return this.http.get(this.xmlUrl, { responseType: 'text' }).pipe(map((xmlString) => this.parseXml(xmlString)));
-    }
+    // getProducts(): Observable<Product[]> {
+    //     return this.http.get(this.xmlUrl, { responseType: 'text' }).pipe(map((xmlString) => this.parseXml(xmlString)));
+    // }
 
-    private parseXml(xmlString: string): Product[] {
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-        const productNodes = xmlDoc.getElementsByTagName('product');
-        const productList: Product[] = [];
+    // private parseXml(xmlString: string): Product[] {
+    //     const parser = new DOMParser();
+    //     const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+    //     const productNodes = xmlDoc.getElementsByTagName('product');
+    //     const productList: Product[] = [];
 
-        // Si el XML está mal formado, normalmente aparece <parsererror>
-        if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
-            return [];
-        }
+    //     // Si el XML está mal formado, normalmente aparece <parsererror>
+    //     if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
+    //         return [];
+    //     }
 
-        for (let i = 0; i < productNodes.length; i++) {
-            const node = productNodes[i];
+    //     for (let i = 0; i < productNodes.length; i++) {
+    //         const node = productNodes[i];
 
-            productList.push({
-                id: parseInt(this.getValue(node, 'id')),
-                name: this.getValue(node, 'name'),
-                price: parseFloat(this.getValue(node, 'price')),
-                image: this.getValue(node, 'image'),
-                category: this.getValue(node, 'category'),
-                quantity: parseInt(this.getValue(node, 'quantity'), 10),
-                description: this.getValue(node, 'description'),
-                inventoryStatus: this.getValue(node, 'inStock') // Mapping <inStock> to inventoryStatus
-            });
-        }
+    //         productList.push({
+    //             id: parseInt(this.getValue(node, 'id')),
+    //             name: this.getValue(node, 'name'),
+    //             price: parseFloat(this.getValue(node, 'price')),
+    //             image: this.getValue(node, 'image'),
+    //             category: this.getValue(node, 'category'),
+    //             quantity: parseInt(this.getValue(node, 'quantity'), 10),
+    //             description: this.getValue(node, 'description'),
+    //             inventoryStatus: this.getValue(node, 'inStock') // Mapping <inStock> to inventoryStatus
+    //         });
+    //     }
 
-        return productList;
-    }
+    //     return productList;
+    // }
 
-    private getValue(parent: Element, tagName: string): string {
-        const element = parent.getElementsByTagName(tagName)[0];
-        return element ? element.textContent || '' : '';
-    }
+    // private getValue(parent: Element, tagName: string): string {
+    //     const element = parent.getElementsByTagName(tagName)[0];
+    //     return element ? element.textContent || '' : '';
+    // }
 }
