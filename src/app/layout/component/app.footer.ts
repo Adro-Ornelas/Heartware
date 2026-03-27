bb43f33c82ca26112c546f932210d9924601e4f7
Adro Ornelas <a22300918@ceti.mx>
2026-03-26 22:01:41 -0600

    Added github on footer hyperlink

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -4,9 +4,11 @@ import { Component } from '@angular/core';
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Heartware</a>
-
+        <a href="https://github.com/Adro-Ornelas/Heartware" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Heartware
+        </a>
+                    
         <img src="images/favicon.webp" alt="Heartware logo" style="height: 40px;margin-left: 20px;" />
+
     </div>`
 })
 export class AppFooter {}
